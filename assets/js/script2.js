61b2ec4e8ee8ebbc4512d529e1df69d2a3118335
takahashi <takahashi@michael-inc.com>
2018-12-04 16:00:38 +0900

    pfs、Labのデザイン修正など

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -2,35 +2,51 @@ $(window).scroll(function(){
 	console.log($(this).scrollTop());
 })
 $(".description1").css("position","static");
+
+// topタイプライター
+$(window).on('load',function(){
+	$('.top-comment').children().andSelf().contents().each(function() {
+		if (this.nodeType == 3) {
+			$(this).replaceWith($(this).text().replace(/(\S)/g, '<span>$1</span>'));
+		}
+	})
+	$('.top-comment').css({'opacity':1});
+	for (var i = 0; i <= $('.top-comment').children().size(); i++) {
+		$('.top-comment').children('span:eq('+i+')').delay(80*i).animate({'opacity':1},50);
+	}
+})
+
+
+// コメント入れ替え
+// 現状だとwindowサイズが変更された時コメントがずれてしまう
 var des1 = $(".description1").html();
 var des2 = $(".description2").html();
 var des3 = $(".description3").html();
-var des4 = $(".description4").html();
 
 $(window).scroll(function(){
-	scr_count = $(this).scrollTop();
-	if(scr_count < 740) {
+	var scr_count = $(this).scrollTop();
+	if(scr_count < 1503) {
 		$(".description1").css("position","static");
 		$(".description1").css("width","65%");
+
 	}
-	if(scr_count > 742) {
+	if(scr_count >= 1503) {
 		$(".description1").css("position","fixed");
 		$(".description1").css("width","52%");
+
 	}
-	if(scr_count < 1400 ) {
-		$(".description1").html(des1);
-	}
-	if(scr_count > 1400 && scr_count < 1860) {
-		$(".description1").html(des2);
-	}
-	if(scr_count > 1860 && scr_count < 2260) {
-		$(".description1").html(des3);
-	}
-	if(scr_count > 2260 && scr_count < 2500) {
-		$(".description1").html(des4);
-	}
-	if(scr_count > 2500) {
+	if(scr_count >= 2460) {
 		$(".description1").css("position","static");
 		$(".description1").css("width","65%");
+
+	}
+	if(scr_count < 1860) {
+		$(".description1").html(des1);
+	}
+	if(scr_count >= 1860 && scr_count < 2310) {
+		$(".description1").html(des2);
+	}
+	if(scr_count >= 2310) {
+		$(".description1").html(des3);
 	}
 })
