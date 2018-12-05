1fa0b33d0e7c02c08e61721f8146cd5e8daea74f
takahashi <takahashi@michael-inc.com>
2018-12-06 00:45:59 +0900

    本番に適用

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,61 @@
-// $(window).on('scroll' , function(){
-//   if ($("#portfolio").is(":hidden")) {
-//     //見えてなかったら
-//     $(this).css('position','static');
-//   } else {
-//     //見えてたら
-//     $(this).css('pos
+$(function() {
+	// $(window).scroll(function(){
+	// 	console.log($(this).scrollTop());
+	// })
+	$(".description1").css("position","static");
 
-// portfolioのdescriptionを固定する
-$(function(){
-	$(window).scroll(function(){
-		var scr_count = $(window).scrollTop();
-		var obj_pos = $("#portfolio").offset().top;
-		var obj_pos_3 = $(".work3").offset().top;
-		// var topHeight = $("#top").outerHeight(true) + $("#profile").outerHeight(true);
-		// var height = $("#portfolio").outerHeight(true);
-		if(scr_count > obj_pos){
-			$(".description").css("position","fixed");
-		} else {
-			$(".description").css("position","static");
-		}
-		if(scr_count > obj_pos_3){
-			$(".description").css("position","static");
+	// navのスムーススクロール
+	$(".nav li").click(function () {
+		var target = $(this).attr("class");
+		var position = $("#"+target).offset().top;
+		$('html,body').animate({ scrollTop: position }, 500);
+		return false;
+	});
+
+
+	// topタイプライター
+	$('.top-comment').children().andSelf().contents().each(function() {
+		if (this.nodeType == 3) {
+			$(this).replaceWith($(this).text().replace(/(\S)/g, '<span>$1</span>'));
 		}
 	})
-})	
+	$('.top-comment').css({'opacity':1});
+	for (var i = 0; i <= $('.top-comment').children().size(); i++) {
+		$('.top-comment').children('span:eq('+i+')').delay(90*i).animate({'opacity':1},50);
+	}
+
+
+	// コメント入れ替え
+	// 現状だとwindowサイズが変更された時コメントがずれてしまう
+	var des1 = $(".description1").html();
+	var des2 = $(".description2").html();
+	var des3 = $(".description3").html();
+
+	$(window).scroll(function(){
+		var scr_count = $(this).scrollTop();
+		if(scr_count < 1890) {
+			$(".description1").css("position","static");
+			$(".description1").css("width","60%");
+
+		}
+		if(scr_count >= 1890) {
+			$(".description1").css("position","fixed");
+			$(".description1").css("width","52%");
+
+		}
+		if(scr_count >= 2950) {
+			$(".description1").css("position","static");
+			$(".description1").css("width","60%");
+
+		}
+		if(scr_count < 2160) {
+			$(".description1").html(des1);
+		}
+		if(scr_count >= 2160 && scr_count < 2610) {
+			$(".description1").html(des2);
+		}
+		if(scr_count >= 2610) {
+			$(".description1").html(des3);
+		}
+	})
+})
