4b5376c4b3e53f3152b5fe4e6f11a1ab6de2cc8a
takahashi <takahashi@michael-inc.com>
2018-12-06 00:43:43 +0900

    デザイン修正

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,10 +1,19 @@
-$(window).scroll(function(){
-	console.log($(this).scrollTop());
-})
-$(".description1").css("position","static");
+$(function() {
+	// $(window).scroll(function(){
+	// 	console.log($(this).scrollTop());
+	// })
+	$(".description1").css("position","static");
 
-// topタイプライター
-$(window).on('load',function(){
+	// navのスムーススクロール
+	$(".nav li").click(function () {
+		var target = $(this).attr("class");
+		var position = $("#"+target).offset().top;
+		$('html,body').animate({ scrollTop: position }, 500);
+		return false;
+	});
+
+
+	// topタイプライター
 	$('.top-comment').children().andSelf().contents().each(function() {
 		if (this.nodeType == 3) {
 			$(this).replaceWith($(this).text().replace(/(\S)/g, '<span>$1</span>'));
@@ -12,41 +21,41 @@ $(window).on('load',function(){
 	})
 	$('.top-comment').css({'opacity':1});
 	for (var i = 0; i <= $('.top-comment').children().size(); i++) {
-		$('.top-comment').children('span:eq('+i+')').delay(80*i).animate({'opacity':1},50);
+		$('.top-comment').children('span:eq('+i+')').delay(90*i).animate({'opacity':1},50);
 	}
-})
 
 
-// コメント入れ替え
-// 現状だとwindowサイズが変更された時コメントがずれてしまう
-var des1 = $(".description1").html();
-var des2 = $(".description2").html();
-var des3 = $(".description3").html();
+	// コメント入れ替え
+	// 現状だとwindowサイズが変更された時コメントがずれてしまう
+	var des1 = $(".description1").html();
+	var des2 = $(".description2").html();
+	var des3 = $(".description3").html();
 
-$(window).scroll(function(){
-	var scr_count = $(this).scrollTop();
-	if(scr_count < 1503) {
-		$(".description1").css("position","static");
-		$(".description1").css("width","65%");
+	$(window).scroll(function(){
+		var scr_count = $(this).scrollTop();
+		if(scr_count < 1890) {
+			$(".description1").css("position","static");
+			$(".description1").css("width","60%");
 
-	}
-	if(scr_count >= 1503) {
-		$(".description1").css("position","fixed");
-		$(".description1").css("width","52%");
+		}
+		if(scr_count >= 1890) {
+			$(".description1").css("position","fixed");
+			$(".description1").css("width","52%");
 
-	}
-	if(scr_count >= 2460) {
-		$(".description1").css("position","static");
-		$(".description1").css("width","65%");
+		}
+		if(scr_count >= 2950) {
+			$(".description1").css("position","static");
+			$(".description1").css("width","60%");
 
-	}
-	if(scr_count < 1860) {
-		$(".description1").html(des1);
-	}
-	if(scr_count >= 1860 && scr_count < 2310) {
-		$(".description1").html(des2);
-	}
-	if(scr_count >= 2310) {
-		$(".description1").html(des3);
-	}
+		}
+		if(scr_count < 2160) {
+			$(".description1").html(des1);
+		}
+		if(scr_count >= 2160 && scr_count < 2610) {
+			$(".description1").html(des2);
+		}
+		if(scr_count >= 2610) {
+			$(".description1").html(des3);
+		}
+	})
 })
